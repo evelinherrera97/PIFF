52fb0b6d494a4f81015ab4f8203bcd00e0c4a10a
evelin.herrera <evelin.herrera@banistmo.com>
2021-11-22 14:43:14 -0500

    heroku

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/piff'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/piff/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
